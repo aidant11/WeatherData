435861418e0ff95160131ab4649b9ad53a857974
aidant11 <aidant10@nycstudents.net>
2025-01-08 16:51:02 +0000

    123

--- WeatherData.java
+++ WeatherData.java
@@ -1,14 +1,32 @@
+import java.util.ArrayList;
 public class WeatherData
 {
     /** Guaranteed not to be null and to contain only non-null entries */
     private ArrayList<Double> temperatures;
-
+    public WeatherData(ArrayList<Double> temps)
+    {
+        temperatures=temps;
+    }
     /**
     * Cleans the data by removing from temperatures all values that are less than
     * lower and all values that are greater than upper, as described in part (a)
     */
     public void cleanData(double lower, double upper)
-    { /* to be implemented in part (a) */ }
+    {
+        for (int i = 0; i<temperatures.size(); i++) 
+        {
+            double temp = temperatures.get(i);
+            if (temp<lower || temp>upper) {
+                temperatures.remove(i);
+                i--;
+            }
+        }
+    }
+
+    public String toString()
+    {
+        return temperatures.toString();
+    }
 
     /**
     * Returns the length of the longest heat wave found in temperatures, as described in
@@ -16,7 +34,17 @@ public class WeatherData
     * Precondition: There is at least one heat wave in temperatures based on threshold.
     */
     public int longestHeatWave(double threshold)
-    { /* to be implemented in part (b) */ }
+    { 
+        int heatWave = 0;
+        int max = 0;
+        for (double t: temperatures)
+        {
+            if (t>threshold) 
+            {
+                heatWave++;
+            }
+        }
+    }
     
     // There may be instance variables, constructors, and methods that are not shown.
 }
